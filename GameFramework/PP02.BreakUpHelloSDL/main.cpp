37d786c0f4c9206f5da5c1e61233bf129d2b912c
성은 박 <20171156@game.hoseo.edu>
2018-09-13 10:37:57 +0900

    -9월 13일 -PP02.BreakUpHelloSDL -라이브러리 폴더 x64로 변경 후 정상작동

--- GameFramework/PP02.BreakUpHelloSDL/main.cpp
+++ GameFramework/PP02.BreakUpHelloSDL/main.cpp
@@ -1,53 +1,34 @@
 #include<SDL.h>
 
-//int main(int argc, char* args[])
-//{
-//
-//    if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
-//    {
-//
-//        g_pWindow = SDL_CreateWindow("PP01.HelloSDL",
-//            SDL_WINDOWPOS_CENTERED,
-//            SDL_WINDOWPOS_CENTERED,
-//            640, 480, SDL_WINDOW_SHOWN);
-//
-//        if (g_pWindow != 0) {
-//            g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-//        }
-//    }
-//    else {
-//        return 1;
-//    }
-//
-//    SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
-//    SDL_RenderClear(g_pRenderer);
-//    SDL_RenderPresent(g_pRenderer);
-//    SDL_Delay(5000);
-//    SDL_Quit();
-//    return 0;
-//} 기능을 분리한다
-
 SDL_Window*g_pWindow = 0;
 SDL_Renderer*g_pRenderer = 0;
 
-void init() {}
-void render() {}
-void update() {}
-void handleEvents() {}
-void clean() {}
+bool g_bRunning = false;
 
-bool g_bRunning = true;
+void render();
+bool init(const char* title, int xpos, int ypos, int width, int height, int flags);
 
-int main()
+int main(int argc, char *argv[])
 {
-    init();
+    if (init("Chapter 1 : Setting up SDL",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        640, 480, SDL_WINDOW_SHOWN))
+    {
+        g_bRunning = true;
+    }
+    else
+    {
+        return 1;
+    }
+
     while (g_bRunning)
     {
-        handleEvents();
-        update();
         render();
     }
-    clean();
+
+    SDL_Delay(10000);
+    SDL_Quit();
     return 0;
 }
 
@@ -77,25 +58,3 @@ void render()
     SDL_RenderPresent(g_pRenderer);
 }
 
-int main(int argc, char *argv[])
-{
-    if (init("Chapter 1 : Setting up SDL",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        640, 480, SDL_WINDOW_SHOWN))
-    {
-        g_bRunning = true;
-    }
-    else
-    {
-        return 1;
-    }
-
-    while (g_bRunning)
-    {
-        render();
-    }
-
-    SDL_Quit();
-    return 0;
-}
