88059858c353d8e57887113075c72f3e5cae9aac
shannon <20171156@game.hoseo.edu>
2018-09-18 13:22:21 +0900

    -test

--- GameFramework/PP04.BasicDrawing/Game.cpp
+++ GameFramework/PP04.BasicDrawing/Game.cpp
@@ -14,6 +14,9 @@ bool Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 		m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
 		SDL_FreeSurface(pTempSurface);
 		SDL_QueryTexture(m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);
+		//2번째:포맷
+		//3번째 : 접근
+		//4/5번째 : 가로, 세로 크기..image의 크기를 저장한다
 	}
 
 	else
@@ -28,16 +31,16 @@ bool Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 
 void Game::render()
 {
-	//SDL_SetRenderDrawColor(m_pRenderer, 0, 255, 255, 255);
-	//SDL_RenderClear(m_pRenderer);
-	//SDL_RenderPresent(m_pRenderer);
 	m_destinationRectangle.x = m_sourceRectangle.x = 0;
 	m_destinationRectangle.y = m_sourceRectangle.y = 0;
-	m_destinationRectangle.w = m_sourceRectangle.w;
-	m_destinationRectangle.h = m_sourceRectangle.h;
+	m_destinationRectangle.w;
+	m_sourceRectangle.w = 200;
+	m_destinationRectangle.h;
+	m_sourceRectangle.h = 200;
+
 
 	SDL_RenderClear(m_pRenderer);
-	SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
+	SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);//3 : 텍스쳐의 범위와 출력될 범위
 	SDL_RenderPresent(m_pRenderer);
 }
 
