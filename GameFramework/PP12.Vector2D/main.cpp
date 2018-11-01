231d52944826aa555519d66f68c67192bac6ebcd
shannon <20171156@game.hoseo.edu>
2018-11-01 18:02:03 +0900

    -2018.11.1 -PP13.KInputHandler

--- GameFramework/PP12.Vector2D/main.cpp
+++ GameFramework/PP12.Vector2D/main.cpp
@@ -9,7 +9,6 @@ int main(int argc, char* argv[])
 {
 
     std::cout << "game init attempt...\n";
-
     if (TheGame::Instance()->init("Chapter 1", 100, 100, 640, 480, false))
     {
         std::cout << "game init success!\n";
