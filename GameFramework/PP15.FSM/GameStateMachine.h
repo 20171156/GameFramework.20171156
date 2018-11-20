f16e0f4ac4e0ac38a8fa95089e057636b25d4b48
shannon <20171156@game.hoseo.edu>
2018-11-20 12:49:47 +0900

    -PP15.FSM -미완성

---
+++ GameFramework/PP15.FSM/GameStateMachine.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "GameState.h"
+
+class GameStateMachine
+{
+public:
+	void changeState(GameState* pState);
+	void popState();
+
+private:
+	GameState* m_currentState;
+	GameState* m_prevState;
+};
